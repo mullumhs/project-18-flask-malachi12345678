c244054b86975260c3d94b44c773bfd391705c40
malachi12345678 <140124190+malachi12345678@users.noreply.github.com>
2024-11-01 10:40:40 +1100

    task3

--- app1.py
+++ app1.py
@@ -1,9 +1,54 @@
-from flask import Flask
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
+@app.route('/calc/<int:num1>/<string:operation>/<int:num2>')
+
+def calculator(num1, operation, num2):
+
+    if operation == "-":
+        result = num1 - num2
+    else:  
+        result = num1 + num2
 
 
+    return f'{num1} {operation} {num2} = {result}'
+
+@app.route('/search')
+
+def search():
+
+    query = request.args.get('q', '')
+
+    category = request.args.get('category', 'all')
+
+    return f'Searching for "{query}" in category: {category}'
+
+
+todos = []
+
+@app.route('/')
+def list_todos():
+    s = ''
+    for i in todos:
+        s = s + i +'<br>'
+    return s
+
+
+@app.route('/add/<task>')
+ 
+def add_todo(task):
+    todos.append(task)
+
+    return redirect('/')
+
+
+
+@app.route('/remove/<int:task_id>')
+
+def delete_todo(task_id):
+    todos.pop(task_id)
+    return redirect('/')
 
 if __name__ == '__main__':
+    app.run(debug=True) 
 
-    app.run(debug=True)
--- app2.py
+++ app2.py
@@ -1,4 +1,39 @@
 from flask import Flask, render_template
+app = Flask(__name__)
+
+
+#base------------------------------------------------------------------------------------------------------
+@app.route('/base')
+def basic():
+
+    return render_template('base.html')
+
+
+
+#greet---------------------------------------------------------------------------------------------------
+@app.route('/greet/<name>')
+def greet(name):
+
+    return render_template('greet.html', name=name)
+
+
+#inventory--------------------------------------------------------------------------------------------------
+@app.route('/inventory')
+def inventory():
+
+    inventory_items = ['apple', 'banana', 'cherry']
+
+    return render_template('inventory.html', inventory=inventory_items)
+
+#inheritance_test-------------------------------------------------------------------------------------------
+@app.route('/test/<message>')
+
+def test(message):
+
+    return render_template('test.html', message=message)
+
+#static_assets-----------------------------------------------------------------------------------------------
+from flask import Flask, render_template, url_for
 
 
 
@@ -6,14 +41,23 @@ app = Flask(__name__)
 
 
 
-@app.route('/basic')
+@app.route('/profile/<username>')
 
-def basic():
+def profile(username):
 
-    return render_template('basic.html')
+    return render_template('profile.html', username=username)
 
 
 
 if __name__ == '__main__':
 
     app.run(debug=True)
+
+
+
+#-------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+
+    app.run(debug=True)
+
+
